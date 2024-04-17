a6c07d209d645d4e372f7e515372f40873818cd8
ningen <ningen361@gmail.com>
2024-04-17 23:12:16 +0900

    feat: added pagenumber

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,11 +4,9 @@ import { zValidator } from '@hono/zod-validator'
 
 import { BlogContents, microcmsClient } from '../cms'
 
-
-
-
 type Props = {
-    blogContents: BlogContents
+    blogContents: BlogContents,
+    pageNumber: number
 }
 
 const renderBlogContent = (blogContent: BlogContents["contents"][number]) => {
@@ -18,30 +16,44 @@ const renderBlogContent = (blogContent: BlogContents["contents"][number]) => {
     </div>
 }
 
+
+const perPage = 10
+
+const pagenator = (totalCount: number, pageNumber: number) => {
+    return {
+        hasNext: totalCount > pageNumber * perPage,
+        hasPrev: pageNumber > 1
+    }
+}
+
 export const TopPage = (props: Props) => {
+    const { hasNext, hasPrev } = pagenator(props.blogContents.totalCount, props.pageNumber)
     return <>
-        <div>
+        <main>
             <p>総件数: {props.blogContents.totalCount}</p>
             <div>
                 { props.blogContents.contents.map(v => {
                     return renderBlogContent(v);
                 })}
             </div>
-        </div>
+            { hasNext ? <a href={`/?page=${props.pageNumber + 1}`}>次のページへ</a> : <></>}
+            { hasPrev ? <a href={`/?page=${props.pageNumber - 1}`}>前のページへ</a> : <></>}
+        </main>
     </>
 }
 
 export default createRoute(zValidator('query', z.object({
-    limit: z.number().default(10),
-    offset: z.number().default(0),
-    page: z.number().default(1),
+    page: z.string().optional().default("1").transform(v => {
+        const page = Number(v);
+        if(page <= 0) return 1
+        return page
+    })
 })), async (c) => {
-    const { page } = c.req.valid("query")
 
-    const limit = 10
-    const offset = limit * (page - 1)
-    const content = await microcmsClient(c.env.MICROCMS_SERVICE_DOMAIN, c.env.MICROCMS_API_KEY).getBlogContents({ limit: limit, offset: offset })
-    return c.render(<TopPage blogContents={content}></TopPage>, {
+    const { page } = c.req.valid("query")
+    const offset = perPage * (page - 1)
+    const content = await microcmsClient(c.env.MICROCMS_SERVICE_DOMAIN, c.env.MICROCMS_API_KEY).getBlogContents({ limit: perPage, offset: offset })
+    return c.render(<TopPage pageNumber={page} blogContents={content}></TopPage>, {
         title: "Ningen mini blog"
     })
 })
