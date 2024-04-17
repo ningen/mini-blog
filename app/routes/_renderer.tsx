4fa3a8dc843a9db70d4a9fd13d8e82eb5cefb672
ningen <ningen361@gmail.com>
2024-04-18 00:09:40 +0900

    replaced css framework

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -4,7 +4,7 @@ export default jsxRenderer(({ children, title }) => {
   return (
     <html>
       <head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
+        <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-vader.css" />
         <title>{title}</title>
       </head>
       <body>
